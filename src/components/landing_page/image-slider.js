1aeedcf19a837ddf4f231c56c48b4d204c0d1cf1
UberStash <expiredserver@gmail.com>
2021-02-26 22:09:50 -0500

    tweaked a-c componentto match desired animated effect

--- src/components/landing_page/image-slider.js
+++ src/components/landing_page/image-slider.js
@@ -6,7 +6,22 @@ function Image() {
     return (
       <div className="App">
         <h1>Image Slider</h1>
-        <AliceCarousel autoPlay autoPlayInterval="3000">
+        <AliceCarousel 
+        // autoWidth={true}
+        autoHeight={true}
+        autoPlay
+        auto
+        // autoPlayControls
+        autoPlayStrategy="none"
+        autoPlayInterval={10000}
+        animationDuration={2000}
+        animationType="slide"
+        infinite
+        touchTracking={true}
+        enableDotsControls
+        disableButtonsControls
+
+        >
       <img src='https://images.unsplash.com/photo-1495651779359-881fde1808a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' className="sliderimg"/>
       <img src='https://images.unsplash.com/photo-1528645602411-bbeb0d69a6de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' className="sliderimg"/>
       <img src='https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1390&q=80' className="sliderimg"/>
